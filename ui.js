a433b192d595efdd8584c66ad11f492f7dfa4325
nomanalam708-collab <nomanalam708@gmail.com>
2026-05-08 08:55:01 -0400

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,7 @@
+const playBtn = document.getElementById('playBtn');
+const menu = document.getElementById('menu');
+
+playBtn.addEventListener('click', () => {
+
+  menu.style.display = 'none';
+});
